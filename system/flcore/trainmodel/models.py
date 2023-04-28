87ce7b4d466a55728215c6450bf96784b051c338
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2023-04-28 17:44:58 +0700

    update model for dropout

--- system/flcore/trainmodel/models.py
+++ system/flcore/trainmodel/models.py
@@ -208,6 +208,45 @@ class AmazonMLP(nn.Module):
 #         x = self.fc(x)
 #         return x
 
+class PFLTI_CNN(nn.Module):
+    def __init__(self, in_features=1,num_classes=10, dim=1024, drop_out=0.2):
+        super().__init__()
+        # self.drop_out = drop_out
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_features,
+                      32,
+                      kernel_size=5,
+                      padding=0,
+                      stride=1,
+                      bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(drop_out),
+            nn.MaxPool2d(kernel_size=(2, 2))
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32,
+                      64,
+                      kernel_size=5,
+                      padding=0,
+                      stride=1,
+                      bias=True),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(2, 2))
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(dim, 512),
+            nn.ReLU(inplace=True)
+        )
+        self.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = torch.flatten(out, 1)
+        out = self.fc1(out)
+        out = self.fc(out)
+        return out
+
 class FedAvgCNN(nn.Module):
     def __init__(self, in_features=1, num_classes=10, dim=1024):
         super().__init__()
